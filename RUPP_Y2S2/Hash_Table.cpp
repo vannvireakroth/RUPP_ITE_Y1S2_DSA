8b5581b4b9fd87b5e7c5955a43dde7ad04fa8be5
vannvireakroth <vannvireakroth57@gmail.com>
2026-05-01 18:52:23 +0700

    commit

--- RUPP_Y2S2/Hash_Table.cpp
+++ RUPP_Y2S2/Hash_Table.cpp
@@ -1,58 +1,91 @@
 #include <iostream>
-#define Array_Size 200
 using namespace std;
 
-string HashTable[Array_Size];
+#define SIZE 200
 
-// Hash function
-int HashFunction(string key) {
-    int hashValue = 0;
-    for (char ch : key) {
-        hashValue += ch; // sum ASCII
+string table[SIZE];
+
+int hashFunction(string key) {
+    int p = 31, hash = 0, power = 1;
+    for (char c : key) {
+        hash = (hash + c * power) % SIZE;
+        power = (power * p) % SIZE;
     }
-    return hashValue % Array_Size;
+    return hash;
 }
 
-// Insert with Linear Probing
-void Insert(string key) {
-    int index = HashFunction(key);
-
-    // Linear probing
-    while (HashTable[index] != "") {
-        index = (index + 1) % Array_Size;
+void insert(string key) {
+    int index = hashFunction(key);
+    while (table[index] != "") {
+        index = (index + 1) % SIZE;
     }
-
-    HashTable[index] = key;
+    table[index] = key;
 }
 
-// Search with Linear Probing
-bool Search(string key) {
-    int index = HashFunction(key);
+bool search(string key) {
+    int index = hashFunction(key);
     int start = index;
-
-    while (HashTable[index] != "") {
-        if (HashTable[index] == key)
+    while (table[index] != "") {
+        if (table[index] == key)
             return true;
-
-        index = (index + 1) % Array_Size;
-
-        // Stop if we loop back
-        if (index == start)
-            return false;
+        index = (index + 1) % SIZE;
+        if (index == start) break;
     }
-
     return false;
 }
 
-int main() {
-    Insert("ck");
-    Insert("ak");
-    Insert("zk");
+void display() {
+    for (int i = 0; i < SIZE; i++) {
+        if (table[i] == "")
+            cout << i << ": -" << endl;
+        else
+            cout << i << ": " << table[i] << endl;
+    }
+}
 
-    if (Search("ak"))
-        cout << "Found\n";
-    else
-        cout << "Not Found\n";
+int main() {
+    int choice;
+    string key;
+
+    do {
+        cout << "\n=== Hash Table Menu ===" << endl;
+        cout << "1. Insert" << endl;
+        cout << "2. Search" << endl;
+        cout << "3. Display" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter key to insert: ";
+                cin >> key;
+                insert(key);
+                cout << "\"" << key << "\" inserted." << endl;
+                break;
+
+            case 2:
+                cout << "Enter key to search: ";
+                cin >> key;
+                if (search(key))
+                    cout << "\"" << key << "\" Found." << endl;
+                else
+                    cout << "\"" << key << "\" Not Found." << endl;
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                cout << "Exiting..." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice. Try again." << endl;
+        }
+
+    } while (choice != 4);
 
     return 0;
 }
